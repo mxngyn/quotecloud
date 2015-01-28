ead3a82332e65ec499141e8f74821f95a641275c
Mai <mxngyn@gmail.com>
2015-01-28 16:37:52 -0500

    AJAX calls

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,63 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  var $spinner = $('#spinner');
+
+  var $editProfile = $('.edit-profile');
+
+  $editProfile.on('submit', 'form.edit-form', function(event) {
+    event.preventDefault();
+    $spinner.show();
+
+    var $target = $(event.target);
+    $.ajax ({
+      type: 'PUT',
+      url: $target.attr('action'),
+      data: $target.serialize()
+    }).done(function(response){
+      $spinner.hide();
+      $target.closest('form').append(response);
+      window.location.href="/edit";
+    })
+  })
+
+  var $likeQuotes = $('.likes');
+
+  $likeQuotes.on('submit', 'form.like-form', function(event) {
+    event.preventDefault();
+    $spinner.show();
+
+    var $target = $(event.target);
+    $.ajax ({
+      type: 'DELETE',
+      url: $target.attr('action'),
+    }).done(function(response){
+      $spinner.hide();
+      $target.closest('.likes').prepend(response);
+      $target.closest('.liked_quote').remove();
+    })
+  })
+
+  var $allQuotes = $('.all-quotes');
+
+  $allQuotes.on('submit', 'form.like-form', function(event) {
+    event.preventDefault();
+    $spinner.show();
+
+    var $target = $(event.target);
+    $target.hide();
+
+    $.ajax({
+      type: 'POST',
+      url: $target.attr('action'),
+      data: $target.serialize()
+    }).done(function(response){
+      $spinner.hide();
+      $target.closest('.like-link').append(response);
+    })
+  })
+
+  $(".sign-up-link").on("click", function(event) {
+    event.preventDefault();
+    $(".sign-up-form").show();
+  })
 });
