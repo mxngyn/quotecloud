938ac10a181f32093781f2946e6264e51bf38a0c
Mai <mxngyn@gmail.com>
2015-01-28 11:18:47 -0500

    Method to access liked quote's author and content

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,5 +1,17 @@
 class Like < ActiveRecord::Base
   # Remember to create a migration!
-  belongs_to :liker, foreign_key: :liker_id, class_name: "User"
+  belongs_to :liker, class_name: "User"
   belongs_to :quotes
+  has_many :liked_quotes, class_name: "Quote", foreign_key: :liked_id
+
+  def quote_author
+    quote = Quote.find(self.quote_id)
+    quote.author
+  end
+
+  def quote_content
+    quote = Quote.find(self.quote_id)
+    quote.content
+  end
+
 end
