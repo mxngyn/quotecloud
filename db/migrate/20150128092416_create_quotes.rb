e3110ca8b455a1eaa73bcce92029b5de0cc52cdd
Mai <mxngyn@gmail.com>
2015-01-28 09:38:59 -0500

    Added content and author column

--- db/migrate/20150128092416_create_quotes.rb
+++ db/migrate/20150128092416_create_quotes.rb
@@ -3,6 +3,8 @@ class CreateQuotes < ActiveRecord::Migration
     create_table :quotes do |t|
       t.integer :submitter_id
       t.integer :category_id
+      t.string :content
+      t.string :author
       t.timestamps
     end
   end
