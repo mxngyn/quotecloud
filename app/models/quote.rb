2a5924b7524bff48d82993727b0790a103a4946f
Mai <mxngyn@gmail.com>
2015-01-28 10:00:18 -0500

    Adjusted associations

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,5 +2,5 @@ class Quote < ActiveRecord::Base
   # Remember to create a migration!
   belongs_to :category
   belongs_to :submitter, class_name: "User"
-  has_many :likers, through: :likes, source: :user
+  has_many :likers, class_name: "Like"
 end
