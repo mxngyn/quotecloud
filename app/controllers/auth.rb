5da4708c4facdca584c085f4a4177c8e9c19dcc7
Mai <mxngyn@gmail.com>
2015-01-28 10:12:31 -0500

    User auth controllers

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -1,11 +1,48 @@
 get '/' do
-  erb :welcome
+  if current_user
+    erb :home
+  end
+  erb :index
 end
 
 get '/login' do
-  
+  erb :'auth/login'
+end
+
+post '/login' do
+  user = User.find_by(email: params[:user][:email])
+  user.try(:authenticate, params[:user][:password])
+  session[:user_id] = user.id
+
+  if request.xhr?
+    erb :'auth/home'
+  else
+    set_error('Login Failed, Please Try Again')
+    redirect "/"
+  end
 end
 
 get '/signup' do
-  
+  erb :'auth/signup'
+end
+
+post '/signup' do
+  p session[:error]
+  user = User.new(params[:user])
+  if user.save
+    session[:user_id] = user.id
+    erb :'home'
+  else
+    present_errors(user.errors.messages)
+    redirect "/signup"
+  end
+end
+
+get '/logout' do
+  session[:user_id] = nil
+  redirect '/'
+end
+
+get '/home' do
+  erb :'home'
 end
