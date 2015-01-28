731730682b98c1c6c8b36116e5efb663619861c7
Mai <mxngyn@gmail.com>
2015-01-28 11:22:41 -0500

    Seed data for categories

---
+++ db/seeds.rb
@@ -0,0 +1,7 @@
+Category.create(title: "Love")
+Category.create(title: "Inspirational")
+Category.create(title: "Funny")
+Category.create(title: "Happiness")
+Category.create(title: "Friendship")
+Category.create(title: "Wisdom")
+Category.create(title: "Misc")
