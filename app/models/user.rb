b385488781eea5df2fd50c507b93290ef5403c64
Mai <mxngyn@gmail.com>
2015-01-28 20:39:38 -0500

    User validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,13 @@ class User < ActiveRecord::Base
   has_many :submitted_quotes, class_name: "Quote", foreign_key: :submitter_id
   has_many :liked_quotes, class_name: "Like", foreign_key: :liker_id
 
+  validates :name, presence: true
+  validates :email, format: {with: /[a-zA-Z]*[1-9]*@[a-z]*.[a-z]{3}/}
+  validates :email, :presence => {:message => "Please provide email"}
+  validates :email, :uniqueness => {:message => "Sorry, that email is already taken"}
+
+
+  def may_delete?(quote)
+    quote.submitter_id == self.id
+  end
 end
