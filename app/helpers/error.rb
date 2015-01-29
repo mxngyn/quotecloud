5907947b06fdb4fe22816a96214e95dc3e951416
Mai <mxngyn@gmail.com>
2015-01-28 20:39:21 -0500

    Error helper methods

--- app/helpers/error.rb
+++ app/helpers/error.rb
@@ -1,3 +1,21 @@
-def set_error(err_msg)
-  session[:error] = err_msg
+def add_error!(error_msg)
+  if session[:errors]
+    session[:errors] << error_msg
+  else
+    session[:errors] = [error_msg]
+  end
+end
+
+def display_errors!
+  errors = session[:errors]
+  session[:errors] = nil
+  errors.join(", ") if errors
+end
+
+def parse_ar_errors_for_display!(messages)
+  messages.each do |key, arr|
+    arr.each do |err|
+      add_error!("#{key.capitalize}: #{err}")
+    end
+  end
 end
